f69c901e598e062ac877cbf556c543dc92bc4a95
evelinajansen <89136827+evelinajansen@users.noreply.github.com>
2021-08-25 15:51:01 +0200

    Wednesday

--- lib/intro.ex
+++ lib/intro.ex
@@ -1,5 +1,67 @@
 defmodule Intro do
-  def hello do
-    :world
+## UPPGIFT ETT
+  def smallest(n1,n2) when n1 < n2, do: n1
+  def smallest(_n1,n2), do: n2
+
+## UPPGIFT TVÅ
+  def largest(n1,n2,n3) when n1 > n2 and n1 > n3, do: n1
+  def largest(n1,n2,n3) when n2 > n1 and n2 > n3, do: n2
+  def largest(_n1,_n2,n3), do: n3
+## UPPGIFT TREE
+  def smallest(n1,n2,n3,n4), do: smallest(smallest(n3,n4),smallest(n1,n2))
+## UPPGIFT FYRE
+  def ticketPrice(age) when age < 18, do: "10"
+  def ticketPrice(age) when age > 64, do: "15"
+  def ticketPrice(age) when age >= 18 and age <= 64, do: "20"
+##UPPGIFT FYVE
+  def nextNumber(num), do: num + 1
+
+##UPPGIFT SIX
+  def factorial(num) do
+    cond do
+      num == 1 ->
+      1
+      num !== 1 ->
+      num*factorial(num - 1)
+    end
   end
+
+## UPPGIFT SEVN ##
+
+## UPPGIFT EIGHT ##
+  #if point towards null > empty
+
+  def isEmpty(list) do
+    [head | _tail] = list
+    cond do
+      head == nil ->
+        true
+      head !== nil ->
+        false
+    end
+  end
+
+## UPPGIFT NINE ##
+
+  def firstOf(list) do
+    [head | _tail] = list
+    head
+  end
+
+## UPPGIFT TEN
+
+def lastOf(list) do
+  [head | tail] = list
+  cond do
+    tail !== [nil] ->
+      lastOf(tail)
+    tail == [nil] ->
+      head
+  end
+  head
+end
+
+
+
+
 end
--- test/intro_test.exs
+++ test/intro_test.exs
@@ -1,7 +1,139 @@
 defmodule IntroTest do
   use ExUnit.Case
+  ## UPPGIFT ETT ##
+    # test "smallest of two when smallest is first" do
+    #   assert Intro.smallest(1,2) == 1
+    # end
 
-  test "greets the world" do
-    assert Intro.hello() == :world
+    # test "smallest of two when smallest is last" do
+    #   assert Intro.smallest(3,2) == 2
+    # end
+
+    # test "smallest of two of equal size" do
+    #   assert Intro.smallest(1337,1337) == 1337
+    # end
+
+
+  ## UPPGIFT TVÅ ##
+    # test "largest of three when largest is first" do
+    #   assert Intro.largest(3,2,1) == 3
+    # end
+
+    # test "largest of three when largest is in the middle" do
+    #   assert Intro.largest(1,4,2) == 4
+    # end
+
+    # test "largest of three when largest is last" do
+    #   assert Intro.largest(3,2,1337) == 1337
+    # end
+
+    # test "largest of three of equal size" do
+    #   assert Intro.largest(6,6,6) == 6
+    # end
+
+
+  ## UPPGIFT TRE ##
+    # test "smallest of two when smallest is first" do
+    #     assert Intro.smallest(1,2,3,4) == 1
+    #   end
+
+    #   test "smallest of two when smallest is last" do
+    #     assert Intro.smallest(4,3,2,1) == 1
+    #   end
+
+    #   test "smallest of two of equal size" do
+    #     assert Intro.smallest(1337,1337,1337,1337) == 1337
+    #   end
+
+  ## UPPGIFT FYRA ##
+
+    # test "ticketPrice under 18" do
+    #   assert Intro.ticketPrice(12) == 10
+    # end
+
+    # test "ticketPrice over 18 under 64" do
+    #   assert Intro.ticketPrice(19) == 20
+    # end
+
+    # test "ticketPrice when 18" do
+    #   assert Intro.ticketPrice(18) == 20
+    # end
+
+    # test "ticketPrice under 18" do
+    #   assert Intro.ticketPrice(64) == 20
+    # end
+
+    # test "ticketPrice over 64" do
+    #   assert Intro.ticketPrice(65) == 15
+    # end
+
+  ## UPPGIFT FYVE ##
+
+    # test "nextNum after 5" do
+    #   assert Intro.nextNumber(5) == 6
+    # end
+
+    # test "nextNum after -40" do
+    #   assert Intro.nextNumber(-40) == -39
+    # end
+
+    # test "nextNum after 200" do
+    #   assert Intro.nextNumber(200) == 201
+    # end
+
+  ## UPPGIFT SIX ##
+
+    # test "factorial of 2" do
+    #   assert Intro.factorial(2) == 2
+    # end
+
+    # test "factorial of 5" do
+    #   assert Intro.factorial(5) == 120
+    # end
+
+    # test "factorial of 8" do
+    #   assert Intro.factorial(8) == 40320
+    # end
+
+## UPPGIFT SEVN ##
+
+## UPPGIFT EIGHT ##
+
+  # test "list is empty" do
+  #   assert Intro.isEmpty([1,2]) == false
+  # end
+
+  # test "list is not empty" do
+  #   assert Intro.isEmpty([nil]) == true
+  # end
+
+## UPPGIFT NINE
+  # test "list head 1" do
+  #   assert Intro.firstOf([1, 4, 5, 7, 7, 1 ,2]) == 1
+  # end
+
+  # test "list head empty" do
+  #   assert Intro.firstOf([nil]) == nil
+  # end
+
+  # test "list head hello" do
+  #   assert Intro.firstOf(["hello", 5]) == "hello"
+  # end
+
+## UPPGIFT TEN
+test "list head 1" do
+    assert Intro.lastOf([1, 4, 5, 7, 7, 1 ,2]) == 2
   end
+
+  test "list head empty" do
+    assert Intro.lastOf([nil]) == nil
+  end
+
+  test "list head hello" do
+    assert Intro.lastOf(["hello", 5, "world"]) == "world"
+  end
+
+
+
+
 end
